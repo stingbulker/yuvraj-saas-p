3bf4f497e3adaeaf4f3433aa74358ea6329dc3b5
Yuvraj Harsh <yuvrajharsh04@gmail.com>
2024-07-20 12:48:02 +0530

    new file:   app/api/auth/[kindeAuth]/route.ts 	modified:   app/components/Navbar.tsx 	new file:   app/dashboard/page.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,15 @@
 import Link from "next/link";
 import { ThemeToggle } from "./theme-toggle";
 import { Button } from "@/components/ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export function Navbar() {
+export async function Navbar() {
+  const { isAuthenticated } = getKindeServerSession();
   return (
     <nav className="border-b bg-background h-[10vh] flex items-center">
       <div className="container flex items-center justify-between">
@@ -12,10 +19,20 @@ export function Navbar() {
 
         <div className="flex items-center gap-x-5">
           <ThemeToggle />
-          <div className="flex items-center gap-x-5">
-            <Button>Sign In</Button>
-            <Button variant="secondary">Sign Up</Button>
-          </div>
+          {(await isAuthenticated()) ? (
+            <LogoutLink>
+              <Button>Log Out</Button>
+            </LogoutLink>
+          ) : (
+            <div className="flex items-center gap-x-5">
+              <LoginLink>
+                <Button>Sign In</Button>
+              </LoginLink>
+              <RegisterLink>
+                <Button variant="secondary">Sign Up</Button>
+              </RegisterLink>
+            </div>
+          )}
         </div>
       </div>
     </nav>
