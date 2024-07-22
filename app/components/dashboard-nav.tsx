06a5e0453a53c4e9424c3545996428a23392c067
Yuvraj Harsh <yuvrajharsh04@gmail.com>
2024-07-22 16:36:18 +0530

    modified:   app/components/dashboard-nav.tsx

--- app/components/dashboard-nav.tsx
+++ app/components/dashboard-nav.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import { cn } from "@/lib/utils";
 import { CreditCard, Home, Settings } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const navItems = [
   { name: "Home", href: "/dashboard", icon: Home },
@@ -7,5 +12,22 @@ export const navItems = [
 ];
 
 export function DashboardNav() {
-  return <nav className="grid items-start gap-2"></nav>;
+  const pathname = usePathname();
+  return (
+    <nav className="grid items-start gap-2">
+      {navItems.map((item, index) => (
+        <Link key={index} href={item.href}>
+          <span
+            className={cn(
+              "group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
+              pathname === item.href ? "bg-accent" : "bg-transparent"
+            )}
+          >
+            <item.icon className="mr-2 h-4 w-4 text-primary" />
+            <span>{item.name}</span>
+          </span>
+        </Link>
+      ))}
+    </nav>
+  );
 }
